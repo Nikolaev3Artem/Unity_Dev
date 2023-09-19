f34cbbc9ab0fcf8ba6b54d21eac781ac8a73f0b9
Nayrextm <72305093+Nayrextm@users.noreply.github.com>
2023-09-19 18:18:40 +0300

    Added Main Menu with buttons and Screen size is fixed

---
+++ Assets/Scripts/Other/SceneLoader.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        Time.timeScale = 1f;
+    }
+
+     public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+
+ }
